1e6b46bf097bc692b1819f8837a0dba0936511d4
Oleksandr Bacherikov <Al.Cash07@gmail.com>
2017-05-19 23:12:28 +0300

    C++ tester is implemented except for checker call.

--- templates/cpp/tester.cpp
+++ templates/cpp/tester.cpp
@@ -1,22 +1,89 @@
 #include <chrono>
 #include <cstdlib>
-#include <exception>
 #include <fstream>
-#include <future>
 #include <iomanip>
 #include <iostream>
 #include <memory>
-#include <string>
 #include <sstream>
-#include <thread>
-#include <utility>
+#include <string>
 #include <vector>
 
-extern void solve(std::istream& in, std::ostream& out);
+#include <signal.h>
+#include <sys/time.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
-void colorOutput(const std::string& output, bool green) {
-    std::cout << (green ? "\033[32;1m" : "\033[31;1m");
-    std::cout << output << "\033[0m";
+const double TIME_LIMIT = 1.5;
+const double CHECKER_TIME_LIMIT = 1.0;
+
+using namespace std;
+
+extern void solve(istream& in, ostream& out);
+
+void runTest(const string& testFileName) {
+    ifstream in(testFileName + ".in");
+    ofstream out(testFileName + ".out");
+    solve(in, out);
+};
+
+// std::getline analog for file descriptors
+bool getline(FILE* file, string& line) {
+    line.clear();
+    while (true) {
+        int c = fgetc(file);
+        if (c == EOF || c == '\n') break;
+        line += c;
+    }
+    return !line.empty();
+}
+
+// return codes:
+//  0 : success
+// -1 : execution failed
+//  1 : runtime error (error string may be TLE indicating time limit exceeded)
+template <class Function>
+int executeProcess(Function f, double timeLimit, string& error) {
+    int pipefd[2];
+    pipe(pipefd);
+    auto pid = fork();
+    if (pid == 0) {  // child process
+        close(pipefd[0]);  // close input pipe
+        dup2(pipefd[1], STDERR_FILENO);  // redirect stderr to output pipe
+        signal(SIGALRM, [](int sigNum) { exit(sigNum); });  // exit after timer
+        itimerval itimer;
+        itimer.it_interval = {0, 0};
+        itimer.it_value.tv_sec = static_cast<time_t>(timeLimit);
+        itimer.it_value.tv_usec = static_cast<suseconds_t>((timeLimit - itimer.it_value.tv_sec) * 1000000);
+        if (setitimer(ITIMER_REAL, &itimer, nullptr) != 0) {
+            exit(SIGKILL);  // failed to start timer
+        }
+        f();  // function is executed here
+        exit(0);
+    } else if (pid < 0) {  // fork failed
+        return -1;
+    }
+    // parent process
+    close(pipefd[1]);  // close output pipe
+    int status = 0;
+    int returnedPid = waitpid(pid, &status, 0);
+    if (returnedPid != pid) {
+        kill(pid, SIGKILL);
+    }
+    FILE* errorPipe = fdopen(pipefd[0], "r");
+    getline(errorPipe, error);  // read stderr message
+    fclose(errorPipe);
+    if (returnedPid == pid && status == 0) {
+        return 0;
+    }
+    if (WEXITSTATUS(status) == SIGALRM) {
+        error = "TLE";
+    }
+    return 1;
+}
+
+void colorOutput(const string& output, bool green) {
+    cout << (green ? "\033[32;1m" : "\033[31;1m");
+    cout << output << "\033[0m";
 }
 
 int main(int argc, const char* argv[]) {
@@ -25,9 +92,6 @@ int main(int argc, const char* argv[]) {
 
     // read the path to tests list file
     string binaryPath = argv[0];
-    if (binaryPath.substr(max(binaryPath.size(), static_cast<size_t>(4)) - 4) == ".exe") {
-        binaryPath = binaryPath.substr(0, binaryPath.size() - 4);
-    }
     ifstream pathFile(binaryPath + "Path");
     string testsFileName;
     pathFile >> testsFileName;
@@ -36,7 +100,7 @@ int main(int argc, const char* argv[]) {
 
     // read test names
     vector<vector<string>> testCases;
-    ifstream testsListInputFile(testsFileName);
+    FILE* testsListInputFile = fopen(testsFileName.data(), "r");
     string line;
     while (getline(testsListInputFile, line)) {
         testCases.emplace_back();
@@ -44,7 +108,7 @@ int main(int argc, const char* argv[]) {
         testCases.back().push_back(line.substr(0, spaceIndex));
         testCases.back().push_back(spaceIndex < line.size() ? line.substr(spaceIndex + 1) : "");
     }
-    testsListInputFile.close();
+    fclose(testsListInputFile);
     auto isSkipped = [&testCases](size_t testIndex) {
         return testCases[testIndex][1] == "SKIPPED";
     };
@@ -54,14 +118,11 @@ int main(int argc, const char* argv[]) {
     // in debug mode, tests are run without validation and updating tests list file
     for (size_t testIndex = 0, testCount = testCases.size(); testIndex < testCount; ++testIndex) {
         if (isSkipped(testIndex)) continue;
-        string testFileName = testsDirectory + testCases[testIndex][0];
-        ifstream in(testFileName + ".in");
-        ofstream out(testFileName + ".out");
-        solve(in, out);
+        cout << testCases[testIndex][0] << ": running" << endl;
+        runTest(testsDirectory + testCases[testIndex][0]);
     }
 #else
     int testsTotal = 0, testsPassed = 0;
-    auto allowedDetachedThreadCount = max(2U, thread::hardware_concurrency()) - 2;
     for (size_t testIndex = 0, testCount = testCases.size();; ++testIndex) {
         if (testIndex < testCount) {
             cout << testCases[testIndex][0] << ": ";
@@ -89,79 +150,47 @@ int main(int argc, const char* argv[]) {
 
         // run the test as a subprocess and measure time
         string testFileName = testsDirectory + testCases[testIndex][0];
-        exception_ptr exceptionPtr = nullptr;
-        auto runTest = [&testFileName, &exceptionPtr]() {
-            ifstream in(testFileName + ".in");
-            ofstream out(testFileName + ".out");
-            try {
-                solve(in, out);
-            } catch (...) {
-                exceptionPtr = current_exception();
-            }
-        };
-        thread thread(runTest);
         auto start = chrono::system_clock::now();
-        auto future = async(launch::async, &thread::join, &thread);
-        auto futureStatus = future.wait_for(chrono::seconds(1));
+        string error;
+        int status = executeProcess([&testFileName](){
+            runTest(testFileName);
+        }, TIME_LIMIT, error);
         auto end = chrono::system_clock::now();
-        auto duration = chrono::duration_cast<chrono::milliseconds>(end - start);
-        double elapsed = static_cast<double>(duration.count()) / 1000;
+        double elapsed = chrono::duration_cast<chrono::duration<double>>(end - start).count();
 
-        // write results to tests list file
         ostringstream resultStream;
         resultStream << fixed << setprecision(3);
-        bool abortTesting = false;
-        if (futureStatus == future_status::timeout) {  // time limit exceeded
-            resultStream << "TLE " << elapsed;
-            thread.detach();
-            if (allowedDetachedThreadCount == 0) {
-                abortTesting = true;
-            } else {
-                --allowedDetachedThreadCount;
-            }
-        } else {
-            if (exceptionPtr) {  // runtime error
-                try {
-                    rethrow_exception(exceptionPtr);
-                } catch (const exception& exception) {
-                    resultStream << "RE " << elapsed << ' ' << exception.what();
-                }
-            } else {  // run checker
-                /*
-                string checkerCommand = binaryPath + "Checker " + testFileName + ".in"
-                    + testFileName + ".out" + testFileName + ".ans";
-#ifdef _MSC_VER
-                shared_ptr<FILE> pipe(_popen(checkerCommand.data(), "r"), _pclose);
-#else
-                shared_ptr<FILE> pipe(popen(checkerCommand.data(), "r"), pclose);
-#endif
-                if (!pipe) throw std::runtime_error("popen() failed!");
-                const int CHUNK_SIZE = 128;
-                char buffer[CHUNK_SIZE];
-                string checkerOutput;
-                while (!feof(pipe.get())) {
-                    if (fgets(buffer, CHUNK_SIZE, pipe.get()) != 0)
-                        checkerOutput += buffer;
-                }
-                 */
+        if (status == 0) {  // run checker
+            string checkerCommand = binaryPath + "Checker " + testFileName + ".in "
+                + testFileName + ".out " + testFileName + ".ans";
+            status = executeProcess([&checkerCommand]() {
+//                execl(checkerCommand.data());
+            }, CHECKER_TIME_LIMIT, error);
+            if (status == 0) {
                 resultStream << "OK " << elapsed;
                 ++testsPassed;
+            } else if (status == 1) {  // either WA or checker crashed
+                if (error == "TLE") {
+                    status = -1;
+                } else {
+                    resultStream << "WA " << elapsed << ' ' << error;
+                }
             }
+        } else if (status == 1) {
+            if (error == "TLE") {
+                resultStream << "TLE " << elapsed;
+            } else {
+                resultStream << "RE " << elapsed << ' ' << error;
+            }
+        }
+        if (status == -1) {
+            resultStream << "JE " << elapsed << " judgement error";
         }
         line = testCases[testIndex][1] = resultStream.str();
         auto spaceIndex = line.find(' ');
         string verdict = line.substr(0, spaceIndex);
         colorOutput(verdict, verdict == "OK");
         cout << line.substr(spaceIndex) << endl;
-
-        if (abortTesting) {
-            colorOutput("ABORTING", false);
-            cout << std::endl;
-            for (; ++testIndex < testCount;) {
-                if (!isSkipped(testIndex)) testCases[testIndex][1] = "UNKNOWN";
-            }
-            --testIndex;  // to make the last iteration that writes to file
-        }
     }
     cout << "========================================\n";
     if (testsPassed == testsTotal) {
